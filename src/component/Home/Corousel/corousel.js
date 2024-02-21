505b8a1299f2bd8dcc81e018e8b4630f641f4c8b
NurmuhammadBozorov <nbazarovv0119@gmail.com>
2024-02-21 15:36:39 +0500

    ..

--- src/component/Home/Corousel/corousel.js
+++ src/component/Home/Corousel/corousel.js
@@ -1,6 +1,5 @@
 import React  from 'react'
 import './corousel.css'
-import { BsArrowRightSquare } from "react-icons/bs";
 const Corousel = () => {
   return (
     <div className='carousel'>
@@ -48,12 +47,30 @@ const Corousel = () => {
                 <p id='text-caraousel'></p>
             </div>
         </div>
-        <div className='carousel-last'>
-            <div className='carousel-last-button'>
-                <button class="btn_1 btn btn-danger d-flex">
-                    <BsArrowRightSquare />
+        <div className='carousel-last container'>
+            <div className='carousel-last-button justify-content-center d-flex'>
+                <button id='right-btn' class="btn_5 btn btn-danger d-flex">
+                    <svg id='right-svg' xmlns="http://www.w3.org/2000/svg"  width="35" height="35" fill="currentColor" class="bi bi-arrow-right-square" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
                     <p>Katologni ko'rish</p>
                 </button>
+                <button id='download-btn' class="btn_5 btn btn-light d-flex">
+                    <svg id='download' xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+                    </svg>
+                    <p >Katologni Yuklab olish</p>
+                </button>
+                <p className='carousel-last-button-p'>Katalogimizda barcha tovarlarimizni ko‘rishingiz mumkin</p>
+                <div className='svg-caraousel'>
+                    <svg id='left' xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                    </svg>
+                    <svg id='right' xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
+                </div>
             </div>
         </div>
     </div>
--- src/component/Home/Home-head/home-head.jsx
+++ src/component/Home/Home-head/home-head.jsx
@@ -2,8 +2,9 @@ import './home-head.css'
 import NavbarHome from '../Navbar/navbar';
 import Corousel from '../Corousel/corousel';
 const HomeHead = () => {
-  return (<>
-    {/* Home-Head ni Navbar qismi */}
+  return (
+    <>
+      {/* Home-Head ni Navbar qismi */}
       <div>
         <NavbarHome/>
       </div>
@@ -26,12 +27,112 @@ const HomeHead = () => {
           </div>
         </div>
       </div>
-      {/* Home-Head Corousel qismi */}
+      {/* Home-Head (2chi qism) Corousel qismi */}
       <div>
         <Corousel/>
       </div>
+      {/* Home-Head 3chi qism */}
+      <div>
+        <div className='home-3'>
+          <div className='home-3-main d-flex'>
+            <div className='home-3-info '>
+              <div className='home-3-info-text '>
+                <h2>"Oleo" pishiriqlari</h2>
+                <p>Oleo margarin va saryog’laridan tayyorlangan psihiriqlarimiz. Bizning Margarinlarimizdan tayyorlangan narsalarni retseptini koring</p>
+                <div className='info-text-move d-flex'>
+                  <svg width="80" viewBox="0 0 113 113" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle opacity="0.2" cx="56.5" cy="56.5" r="56.5" transform="matrix(-1 0 0 1 113 0)" fill="#D72A33"></circle>
+                    <path d="M32.3011 52.9084C30.1278 54.1459 30.1278 57.2398 32.3011 58.4773L67.7527 78.6644C69.926 79.902 72.6426 78.3551 72.6426 75.88L72.6426 35.5057C72.6426 33.0306 69.926 31.4837 67.7527 32.7213L32.3011 52.9084Z" fill="#D72A33"></path>
+                  </svg>
+                  <h5>Keyingi taom</h5>
+                </div>
+                <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimg_7.dfb2768d.png&w=640&q=75" alt="" srcset="" />
+              </div>
+              
+            </div>
+            <div className='home-3-info-box d-flex'>
+                <div className='info-box'style={{position:'relative'}} >
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div>
+                <div className='info-box'style={{position:'relative'}} >
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div>
+                <div className='info-box'style={{position:'relative'}} >
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div>
+                <div className='info-box' style={{position:'relative'}}>
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div>
+                {/* <div className='info-box' style={{position:'relative'}}>
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div> */}
+                {/* <div className='info-box' style={{position:'relative'}}>
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div>
+                <div className='info-box' style={{position:'relative'}}>
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div>
+                <div className='info-box' style={{position:'relative'}}>
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div>
+                <div className='info-box' style={{position:'relative'}}>
+                  <img src="https://oleouz.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage.212b2072.png&w=3840&q=75" alt="" srcset="" />
+                  <div className='info-box-text'>
+                    <h5>Saryog'</h5>
+                    <p>olio mazzali saryog'lari</p>
+                    <p></p>
+                  </div>
+                </div> */}
+
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className='home-4'>
+
+      </div> */}
     </>
   )
 }
 
-export default HomeHead
+export default HomeHead;
